c2e6f2b56e4f9b400dca56d0664f66be5abdd3d5
MoMoon-LKH <rlgh28@naver.com>
2023-05-31 05:06:01 +0900

    개발중 안내 페이지 style 수정

--- gameinfo/src/errorPage/CurrentDevelopment.jsx
+++ gameinfo/src/errorPage/CurrentDevelopment.jsx
@@ -3,7 +3,8 @@
 const CurrentDevelopment = () => {
 
     return (
-        <div style={{width: '100%', marginTop: '200px', textAlign: 'center', fontSize: '25px', fontWeight: 'bold'}}>
+        <div style={{display: 'flex', width: '100%', height: '400px', 
+        alignItems: 'center', justifyContent: 'center', fontSize: '25px', fontWeight: 'bold'}}>
                 해당 페이지는 개발 중 입니다
         </div>
     );
