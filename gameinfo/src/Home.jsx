059ebcf38e3b91b27ef555aafa020a67d6776bde
MoMoon-LKH <rlgh28@naver.com>
2023-06-02 02:27:52 +0900

    메인화면 뉴스 이미지 리스트 부분 화면 작성

--- gameinfo/src/Home.jsx
+++ gameinfo/src/Home.jsx
@@ -6,13 +6,26 @@ import axios from "axios"
 import customAxios from "./config/ApiUrl"
 
 const Home = () => {
+
+    const sampleList = [
+        {
+            id: 1,
+            title: 'title1'
+        }, 
+        {
+            id: 2,
+            title: 'title2'
+        }
+    ]
+
     
     const [imgSeq, setImgSeq] = useState(0)
     const [newsImage, setNewsImage] = useState({})
-    const [newsImageList, setNewsImageList] = useState([])
+    const [newsImageList, setNewsImageList] = useState(sampleList)
     const [newsList, setNewsList] = useState([])
     const [reviewList, setReviewList] = useState([])
 
+    
 
     const getMainList = () => {
         customAxios.get("/main/list")
@@ -25,27 +38,12 @@ const Home = () => {
     }
 
     useEffect(() => {
-
+        setNewsImage(newsImageList[0])
     }, [])
 
-    const onClickImageNext = (seq) => {
-        if(seq >= newsImageList.length) {
-            setImgSeq(0)
-        } else {
-            setImgSeq(imgSeq + 1)
-            
-        }
-        setNewsImage(newsImageList[imgSeq])
-    }
+    const onHoverNewsImageList = (i) => {
+        setNewsImage(newsImageList[i])
 
-    const onClickImagePrev = (seq) => {
-        if(seq < 0) {
-            setImgSeq(newsImageList.length - 1)
-        } else {
-            setImgSeq(imgSeq - 1)
-        }
-
-        setNewsImage(newsImageList[imgSeq])
     }
 
     return(
@@ -54,15 +52,31 @@ const Home = () => {
                 <div className="post-bar-border">
                     <div className="post-bar-text">뉴스</div>    
                 </div>
-                <div className="image-list" style={{widht: '100%', backgroundColor: 'black', height:'350px'}}>
-                    <div className="image-content left">
+                <div className="image-list" style={{widht: '100%', height:'450px', position: 'relative'}}>
+                    <div className="image-items">
+                        {newsImageList.map((news, index) =>
+                            <div className="image-item" key={index} onMouseEnter={() => onHoverNewsImageList(index)}>
+                                {news.title}
+                            </div>
+                        )
+                        
+                        }
+                    </div>
+                    <div className="image-content main" style={
+                        {
+                            backgroundColor: 'lightgray',
+                            //backgroundImage: 'url(https://gameinfo.momoon.kro.kr/iamges/' + newsImage.imageName,
+                            height: '85%',
+                            display: 'flex',
+                            flexDirection: 'column'
+                        }
+                        }>
                         
                     </div>
-                    <div className="image-content main" style={{backgroundImage: 'url(https://gameinfo.momoon.kro.kr/iamges/${newsImage.image})'}}>
-                        
-                    </div>
-                    <div className="image-content right">
-                        
+                    <div className="image-content-text">
+                        <div className="image-text">
+                            {newsImage.title}
+                        </div>
                     </div>
                 </div>
                 <div style={{width: '100%', border: '1px solid gray', padding:'10px'}}>
